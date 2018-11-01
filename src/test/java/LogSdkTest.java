6718c995774849b0dc65e22d39063a4e93695206
cxc <1083190363@qq.com>
2018-11-01 09:16:38 +0800

    Log

--- src/test/java/LogSdkTest.java
+++ src/test/java/LogSdkTest.java
@@ -2,7 +2,11 @@ import com.qianfeng.sdk.LogSdk;
 
 import java.io.UnsupportedEncodingException;
 
+/**
+ * 测试类
+ */
 public class LogSdkTest {
+
     public static void main(String[] args) {
         try {
             System.out.println(LogSdk.chargeSuccess("zhangsan-999"
